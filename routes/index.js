b9fdc61b6b64fa04cb3101845cf781433785129f
Caitlin <rubinca@sas.upenn.edu>
2016-11-24 01:01:54 -0500

    trianglify as background

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/resume', function(req, res, next) {
+	res.render('resume')
+})
+
+router.get('/contact', function(req, res, next) {
+	res.render('contact')
+})
 module.exports = router;
